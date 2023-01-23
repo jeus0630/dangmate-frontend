0473cf3f984d902e14c7428f81d07ff31c18c725
Jewoo <jeus0630@gmail.com>
2023-01-24 00:15:21 +0900

    Add AppBarBack component

---
+++ src/components/AppBarBack.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let appBarBackTitle: string = 'Title';
+</script>
+
+<div class="flex items-center text-t1">
+	<div class="img-wrap">
+		<button>
+			<img src="back-arrow.svg" alt="" />
+		</button>
+	</div>
+	<div class="content text-t2 pl-[1.6rem] text-gray-sub-head">
+		<span>{appBarBackTitle}</span>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
 	import '../app.css';
 	import AppBar from '../components/AppBar.svelte';
+	import AppBarBack from '../components/AppBarBack.svelte';
 </script>
 
 <header class="p-[2rem]">
-	<AppBar />
+	<AppBarBack />
 </header>
 <main class="px-[2rem]">main</main>
 <footer class="px-[2rem]">footer</footer>
