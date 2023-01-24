c983cef73940b0bb35826460398f25579ce8be3f
Jewoo <jeus0630@gmail.com>
2023-01-24 18:55:03 +0900

    Add Post Component

---
+++ src/components/Post.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	export let postImage: string;
+	export let profileImage: string;
+	export let userName: string;
+	export let category: string;
+	export let content: string;
+	export let location: string;
+	export let updatedAt: string;
+	export let numOfComments: number;
+	export let numOfLikes: number;
+</script>
+
+<div class="item flex flex-col">
+	<div class="mb-[2.1rem]">
+		<figure class="w-full">
+			<img src={postImage} alt="" />
+		</figure>
+	</div>
+
+	<div class="flex items-center justify-between">
+		<div class="left flex items-center">
+			<div class="profile w-[1.8rem] h-[1.8rem] rounded-[50%] overflow-hidden">
+				<img src={profileImage} alt="" />
+			</div>
+			<em class="user inline-block ml-[0.6rem] font-bold text-t3">{userName}</em>
+		</div>
+
+		<div class="right">
+			<div
+				class="category flex justify-center items-center px-[1rem] py-[0.3rem] border-[0.1rem] rounded-[1.6rem] border-primary-emphasys text-primary-emphasys"
+			>
+				<span>{category}</span>
+			</div>
+		</div>
+	</div>
+
+	<div class="text mt-[2.8rem] mb-[3rem]">
+		<p class="text-b2 ">{content}</p>
+	</div>
+
+	<div class="bottom flex justify-between items-center text-btn2">
+		<div class="left flex">
+			<div class="location mr-[1.2rem]">
+				<span>{location}</span>
+			</div>
+			<div class="time">
+				<span>{updatedAt}</span>
+			</div>
+		</div>
+		<div class="right flex">
+			<div class="comment flex items-center">
+				<button class="mr-[0.5rem]">
+					<img src="empty-comment.svg" alt="" />
+				</button>
+				<span>{numOfComments}</span>
+			</div>
+			<div class="like flex items-center ml-[1.2rem]">
+				<button class="mr-[0.5rem]">
+					<img src="empty-heart.svg" alt="" />
+				</button>
+				<span>{numOfLikes}</span>
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,27 @@
 	import AppBar from '../components/AppBar.svelte';
 	import AppBarBack from '../components/AppBarBack.svelte';
 	import ButtonCategory from '../components/ButtonCategory.svelte';
+	import Post from '../components/Post.svelte';
 	import TextField from '../components/TextField.svelte';
 	import TextFieldError from '../components/TextFieldError.svelte';
 	import TextFieldGood from '../components/TextFieldGood.svelte';
+
+	const props = {
+		postImage: 'https://mungmate-bucket.s3.ap-northeast-2.amazonaws.com/pIHnaBuhMM',
+		profileImage: 'https://mungmate-bucket.s3.ap-northeast-2.amazonaws.com/pIHnaBuhMM',
+		userName: '말괄량이 조이',
+		category: '댕댕 이야기',
+		content:
+			'OO동에 이사온지 얼마 되지 않아 아는 친구가 한 명도 없어요ㅜㅜ나이는 25살 여자입니다! 같이 밥먹고 운동 다닐 동네 친구 구해요!또래에 산책 같이 마음 맞는 분 찾으면 좋겠어요!',
+		location: '공덕동',
+		updatedAt: '방금전',
+		numOfComments: 26,
+		numOfLikes: 26
+	};
 </script>
 
 <header class="p-[2rem]">
-	<!-- <TextFieldError {...props} /> -->
+	<Post {...props} />
 </header>
-<main class="px-[2rem]">main</main>
+<main class="px-[2rem] text-l2">main</main>
 <footer class="px-[2rem]">footer</footer>
